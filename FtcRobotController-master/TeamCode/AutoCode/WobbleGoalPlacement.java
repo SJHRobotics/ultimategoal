ea5be9001658acc41b99a24d698470289c49d806
Nikilesh-K <ramskashyap@gmail.com>
2021-02-03 17:09:31 -0500

    Removed Stop() call from color sensing if statement

--- FtcRobotController-master/TeamCode/AutoCode/WobbleGoalPlacement.java
+++ FtcRobotController-master/TeamCode/AutoCode/WobbleGoalPlacement.java
@@ -68,7 +68,6 @@ public class WobbleGoalPlacement extends LinearOpMode{
             MoveForward(1000);
             Stop();
             if(CSensor.red() > CSensor.blue() && CSensor.red() > CSensor.green()){
-                Stop();
                 System.out.println("I am at Target Zone!");
                 break;
             }
