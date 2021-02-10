7768ea87cdff112987e19f7614bc379e62eb8400
Nikilesh-K <ramskashyap@gmail.com>
2021-02-10 18:22:50 -0500

    Increased sleep value

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/WobbleGoalPlacement.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/WobbleGoalPlacement.java
@@ -232,10 +232,7 @@ public class WobbleGoalPlacement extends LinearOpMode{
             // Drive till 2nd blue line
             SenseColor('B');
             // Release wobble Goal
-            PlaceWobbleGoal(1000);
-            // Move forward 1 sec
-            MoveForward();
-            sleep(1000);
+            PlaceWobbleGoal(2000);
             //Turn Right to ready to go to white line
             TurnRight(1000);
             // Move to white line
