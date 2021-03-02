bc5d8f2343d10a6deb3519578049910bd86c0e08
Nikilesh-K <ramskashyap@gmail.com>
2021-03-02 17:38:50 -0500

    Created State Machine version of WGP OpMode

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/StateMachineWGP.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+@Autonomous(name = "State Machine Version WGP", group = "WobbleGoal")
+
+public class StateMachineWGP extends LinearOpMode{
+    //Hardware objects
+    private ColorSensor CSensor;
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+    //TFOD Variables
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+    private char targetZone;
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+    private static final String VUFORIA_KEY =
+            "Ae/YeOf/////AAABmR8KMKVXi0gFg1/JtSBMj5WHZwOHCMtdvkRRmVdKQcjYBCk/JBHyLtxgccLh2ZJezNZ2W/ZU6mi38O6dsGABJtKELx/nxVc78up34+6k21SQSPKu8qgK9RuK5deUYb9K9gk8QG9xuGvGD5xQpH+nxeywwwQQXmExoEeLvlp6+H5Qa90lDZZPs2llKVqvdmuA8TSpGEktHgLcH0L4QtnF1JM1e7GY6woBW3aktTjXtqjK9mtvgbTRuBceBeLUuy7nhrT2+qt7aPzSAWsMgvrdduScWpYl14bQESUVEWX6Dz8xcNHOsDVnPB593nqj2KVVBbcHno8NATIGDvERkE2d4SUa5IRECzJ+nWbI9Fcx3zdZ";
+
+
+    //Movement methods
+    public void MoveForward(){
+        //Set frontLeft motor power level slightly higher than others
+        //This is to offset drift in our robot's movement
+        frontLeftMotor.setPower(0.32);
+        frontRightMotor.setPower(0.3);
+        backLeftMotor.setPower(0.3);
+        backRightMotor.setPower(0.3);
+    }
+    public void MoveBackward(){
+        frontLeftMotor.setPower(-0.32);
+        frontRightMotor.setPower(-0.3);
+        backLeftMotor.setPower(-0.3);
+        backRightMotor.setPower(-0.3);
+    }
+    public void TurnRight(long time){
+        frontLeftMotor.setPower(1.0);
+        frontRightMotor.setPower(-1.0);
+        backLeftMotor.setPower(1.0);
+        backRightMotor.setPower(-1.0);
+        sleep(time);
+    }
+    public void TurnLeft(long time){
+        frontLeftMotor.setPower(-1.0);
+        frontRightMotor.setPower(1.0);
+        backLeftMotor.setPower(-1.0);
+        backRightMotor.setPower(1.0);
+        sleep(time);
+    }
+    public void Stop(){
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+    }
+
+    //For initializing Vuforia
+    private void initVuforia() {
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "TensorflowWebcam");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    //For initalizing TFOD
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+
+
+    //This method programs the robot to drive forward or backward until the Color Sensor(referred to as CSensor) has detected white or blue
+    //color parameter is for the color robot needs to stop at, direction parameter is the direction robot should drive
+    // FOR COLOR PARAMETER: 'W' = white, 'B' = blue
+    // FOR DIRECTION PARAMETER: 'F' = forward, 'B' = backward
+    public void SenseColor(char color, char direction){
+
+        //Sense blue color(for Target Zones)
+        if(color == 'B'){
+            while (true) {
+
+                if(direction == 'F'){
+                    MoveForward();
+                }
+                if(direction == 'B'){
+                    MoveBackward();
+                }
+                //If CSensor detects blue line(more blue than other colors)
+                if (CSensor.blue() > CSensor.red() && CSensor.blue() > CSensor.green()) {
+                    Stop();
+                    break;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        //Sense white color(for Launch Line)
+        if(color == 'W'){
+            while (true) {
+
+                if(direction == 'F'){
+                    MoveForward();
+                }
+                if(direction == 'B') {
+                    MoveBackward();
+                }
+
+                //If CSensor detects white(green value is always higher than 5000 when it detects white)
+                if (CSensor.green() > 5000) {
+                    Stop();
+                    break;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void runOpMode(){
+
+    }
+} // end class
